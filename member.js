25bd9950ac7fc5dd219721ee827ae82c49dfccb5
renzedu <renzedu@gmail.com>
2023-12-11 19:14:14 +0000

    Copilot second commit git push

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember(){
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    skills.style.backgroundColor = "#fff";
+    member.style.backgroundColor = "#f1f1f1";
+    memberList.style.display = "block";
+    skillsList.style.display = "none";
+}
